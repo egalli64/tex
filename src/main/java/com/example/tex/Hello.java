b06cd76d7fb69815de656ea899074d9d34f0d370
Emanuele Galli <egalli64@gmail.com>
2021-03-21 12:54:36 +0100

    First commit

---
+++ src/main/java/com/example/tex/Hello.java
@@ -0,0 +1,26 @@
+package com.example.tex;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+    private static final Logger log = LogManager.getLogger("Hello");
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.trace("called");
+        response.getWriter().append("Hello @" + LocalDateTime.now());
+    }
+}
